19fed77270007dcf632b789aba5843f58e40b21a
Westley <eugenewestley95@gmail.com>
2026-09-23 23:13:21 +0300

    Deleted former styling, fonts & added a new font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,23 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-<style>
-@import url('https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Fraunces:ital,opsz,wght@0,9..144,100..900;1,9..144,100..900&family=Manrope:wght@200..800&family=Monoton&display=swap');
-</style>
+
 import "@/app/styles/globals.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
-  title: "Eugene Westley",
-  description: "Portfolio",
+  title: "Eugene Westley Mwambacha | Dev & Voice-Over Artist",
+  description: "Frontend Engineer, Voice-Over Artist & Podcast Host",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        suppressHydrationWarning={true}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
